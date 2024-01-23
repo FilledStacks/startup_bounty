e7b4ac680cce9e1627c9ba77388cc97f5854a139
Sujoychandradas <sujoy.chandradas@gmail.com>
2024-01-23 20:13:26 +0600

    Task Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 const bool DRIVE_MODE = bool.fromEnvironment('DRIVE_MODE');
+const useFlutterDriver = true;
 
 Future<void> main() async {
   // TASK: Read this value from a local storage
-  final useFlutterDriver = true;
-
   if (!useFlutterDriver) {
     WidgetsFlutterBinding.ensureInitialized();
   } else {
     enableFlutterDriverExtension();
   }
 
-  runApp(const MyApp());
+  // Read the value from local storage
+  final filePath = await getFilePath();
+  print('File path: $filePath');
+
+  final savedValue = await readValueFromFile(filePath);
+  print('Saved value: $savedValue');
+  runApp(MyApp(savedValue));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String savedValue;
 
-  // This widget is the root of your application.
+  const MyApp(this.savedValue, {super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,15 +36,16 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page', savedValue: savedValue),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.title, required this.savedValue});
 
   final String title;
+  final String savedValue;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -45,11 +54,19 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    // TASK: Change the value here and save
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the counter with the saved value
+    _counter = int.tryParse(widget.savedValue) ?? 0;
+  }
+
+  Future<void> _incrementCounter() async {
+    // Incrementing the counter and saving the new value
     setState(() {
       _counter++;
     });
+    writeValueToFile(await getFilePath(), '$_counter');
   }
 
   @override
@@ -79,5 +96,35 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+}
+
+Future<String> getFilePath() async {
+  String documentsDirectory = (await getApplicationDocumentsDirectory()).path;
+  return '$documentsDirectory/data.txt';
+}
+
+Future<String> readValueFromFile(String filePath) async {
+  try {
+    File file = File(filePath);
+    if (await file.exists()) {
+      String contents = await file.readAsString();
+      return contents;
+    } else {
+      print('File does not exist.');
+      return '0';
+    }
+  } catch (e) {
+    print('Error reading file: $e');
+    return '0';
+  }
+}
+
+Future<void> writeValueToFile(String filePath, String value) async {
+  try {
+    File file = File(filePath);
+    await file.writeAsString(value);
+  } catch (e) {
+    print('Error writing to file: $e');
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,8 +13,8 @@ import 'package:startup_bounty/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
+      await tester.pumpWidget(const MyApp());
+      
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
