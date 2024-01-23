6709710a4dc84716ae490864574698dd65a6450d
predatorx7 <smushaheed@outlook.com>
2024-01-24 00:06:25 +0530

    Add windows and web implementations to find platform's official document directory

---
+++ lib/config/config_io.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:path/path.dart' as path;
+import 'config.dart' show AppConfig;
+
+class _AppConfigIO extends AppConfig {
+  final File _configJsonFile;
+  Map<String, Object?> _cachedConfig = {};
+
+  _AppConfigIO._(this._configJsonFile);
+
+  @override
+  T? getValue<T extends Object>(String key) {
+    return _cachedConfig[key] as dynamic;
+  }
+
+  @override
+  Future<void> setValue(String key, Object? value) async {
+    _cachedConfig[key] = value;
+    await _update();
+  }
+
+  Future<void> _update() async {
+    await _configJsonFile.writeAsString(json.encode(_cachedConfig));
+  }
+
+  Future<void> _load() async {
+    final content = await _configJsonFile.readAsString();
+    if (content.isEmpty) return;
+    _cachedConfig = json.decode(content);
+  }
+}
+
+/// Returns Application documents directory for a platform before `WidgetsFlutterBinding.ensureInitialized` or `runApp`
+/// is used.
+Future<Directory> getPlatformDocumentsDirectoryPath(
+  String applicationPackageName,
+) async {
+  if (Platform.isAndroid) {
+    // This can be different if new accounts were created on android
+    const userId = 0;
+    return Directory('/data/user/$userId/$applicationPackageName');
+  } else if (Platform.isMacOS) {
+    final applicationHomeDir = Platform.environment['HOME'];
+    return Directory('$applicationHomeDir/Documents');
+  } else if (Platform.isIOS) {
+    final bundleContainerDir = File(Platform.executable).parent;
+    // This isn't documents directory but the bundle container directory of this application.
+    // Only works on iOS simulator and will not work on a real iPhone
+    return bundleContainerDir.parent;
+  } else if (Platform.isWindows) {
+    // For storing app config locally for a user, windows had in the reference document to use the env variable LOCALAPPDATA.
+    // Reference: https://web.archive.org/web/20120905053951/http://download.microsoft.com/download/e/6/a/e6aa654f-cccb-421e-9b50-3392e9886084/VistaFileSysNamespaces.pdf
+    final userLocalDataDir = Platform.environment['LOCALAPPDATA'];
+    if (userLocalDataDir == null) throw Exception('Unexpected exception');
+    final lastIndexOfDot = applicationPackageName.lastIndexOf('.');
+    final name = applicationPackageName.substring(lastIndexOfDot + 1);
+    final parent = applicationPackageName.substring(0, lastIndexOfDot);
+    // This is how path_provider makes app support data directory path: com.example.app_name as com.example/app_name
+    final appLocalpathName = path.join(userLocalDataDir, parent, name);
+    return Directory(appLocalpathName);
+  }
+  // return getPlatformDocumentsDirectoryPathFromPlugin();
+  throw UnimplementedError(
+    'Default documents directory path was not setup for other platforms',
+  );
+}
+
+/// Returns path of config file that is inside application's official document directory
+Future<File> getApplicationConfigFile(String packageName) async {
+  final documentsDirectory = await getPlatformDocumentsDirectoryPath(
+    packageName,
+  );
+  await documentsDirectory.create(recursive: true);
+  final configJsonFile = File(
+    path.join(documentsDirectory.path, 'app_config.json'),
+  );
+  if (!await configJsonFile.exists()) {
+    await configJsonFile.create();
+  }
+  return configJsonFile;
+}
+
+Future<AppConfig> getAppConfig(String packageName) async {
+  final file = await getApplicationConfigFile(packageName);
+  return _AppConfigIO._(file).._load();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,22 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
-import 'package:path/path.dart' as path;
-// import 'package:path_provider/path_provider.dart' as path_provider;
+
+import 'config/config.dart';
 
 const bool DRIVE_MODE = bool.fromEnvironment('DRIVE_MODE');
+
 const applicationPackageName = 'com.example.startup_bounty';
 
-AppConfig? configFile;
+late final AppConfig configFile;
 
 const useFlutterDriverKey = 'use_flutter_driver';
 
 Future<void> main() async {
-  configFile = await AppConfig.getConfig(applicationPackageName);
+  configFile = await AppConfig.getAppConfig(applicationPackageName);
+
   // TASK: Read this value from a local storage
   final useFlutterDriver =
-      configFile?.getValue<bool>(useFlutterDriverKey) == true;
-
-  // Observe value from config file here
-  print({'useFlutterDriver': useFlutterDriver});
+      configFile.getValue<bool>(useFlutterDriverKey) == true;
 
   if (!useFlutterDriver) {
     WidgetsFlutterBinding.ensureInitialized();
@@ -67,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
     // update value in the same config file
-    await configFile!.setValue('use_flutter_driver', true);
+    await configFile.setValue('use_flutter_driver', true);
   }
 
   @override
@@ -90,8 +86,9 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             ListTile(
               title: Text(
-                  configFile?.getValue<bool>(useFlutterDriverKey)?.toString() ??
-                      '-'),
+                (configFile.getValue<bool>(useFlutterDriverKey) == true)
+                    .toString(),
+              ),
               subtitle: const Text('use_flutter_driver'),
             ),
           ],
@@ -104,93 +101,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
-
-// Future<Directory> getPlatformDocumentsDirectoryPathFromPlugin() {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   return path_provider.getApplicationDocumentsDirectory();
-// }
-
-/// Returns Application documents directory for a platform before `WidgetsFlutterBinding.ensureInitialized` or `runApp`
-/// is used.
-Future<Directory> getPlatformDocumentsDirectoryPath(
-  String applicationPackageName,
-) async {
-  if (Platform.isAndroid) {
-    return Directory('/data/user/0/$applicationPackageName');
-  } else if (Platform.isMacOS) {
-    final applicationHomeDir = Platform.environment['HOME'];
-    return Directory('$applicationHomeDir/Documents');
-  } else if (Platform.isIOS) {
-    final bundleContainerDir = File(Platform.executable).parent;
-    // This isn't documents directory but the bundle container directory of this application.
-    // Only works on iOS simulator and will not work on a real iPhone
-    return bundleContainerDir.parent;
-  }
-  throw UnimplementedError(
-      'Default documents directory path was not setup for other platforms');
-}
-
-class AppConfig {
-  final File _configJsonFile;
-  final Map<String, Object?> _cachedConfig;
-
-  AppConfig._(this._configJsonFile, this._cachedConfig);
-
-  static Future<AppConfig?> getConfig(String applicationPackageName) async {
-    final file = await getApplicationConfigFile(applicationPackageName);
-    print({
-      "config file path": file.absolute.path,
-    });
-    final content = await file.readAsString();
-    final Map<String, Object?> contentJson =
-        content.isEmpty ? {} : json.decode(content);
-    return AppConfig._(file, contentJson);
-  }
-
-  T? getValue<T extends Object>(String key) {
-    return _cachedConfig[key] as dynamic;
-  }
-
-  Future<void> setValue(String key, Object? value) async {
-    _cachedConfig[key] = value;
-    await _update();
-  }
-
-  Future<void> _update() async {
-    await _configJsonFile.writeAsString(json.encode(_cachedConfig));
-  }
-}
-
-class AppConfigSetupFailure extends Error {
-  final String message;
-  AppConfigSetupFailure(this.message);
-
-  @override
-  String toString() {
-    return 'AppConfigSetupFailure: $message';
-  }
-}
-
-/// Returns path of config file that is inside application's official document directory
-Future<File> getApplicationConfigFile(String applicationPackageName) async {
-  try {
-    final documentsDirectory =
-        await getPlatformDocumentsDirectoryPath(applicationPackageName);
-    print({
-      'application documents dir': documentsDirectory.absolute.path,
-    });
-    await documentsDirectory.create(recursive: true);
-    final configJsonFile = File(
-      path.join(documentsDirectory.path, 'app_config.json'),
-    );
-    if (!await configJsonFile.exists()) {
-      await configJsonFile.create();
-    }
-    return configJsonFile;
-  } catch (e, s) {
-    debugPrint(e.toString());
-    debugPrintStack(stackTrace: s);
-  }
-  throw AppConfigSetupFailure('Could not find app config file directory');
 }
