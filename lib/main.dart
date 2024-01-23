af355a64cb0b87b22a47b6ec59783360630c59d7
josteveadekanbi <adekanbijosteve@gmail.com>
2024-01-23 16:49:51 +0000

    Attempt with temp dir

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
+// add path provider
+import 'package:path_provider/path_provider.dart';
 
 const bool DRIVE_MODE = bool.fromEnvironment('DRIVE_MODE');
 
 Future<void> main() async {
   // TASK: Read this value from a local storage
-  final useFlutterDriver = true;
+  final value = readValueSync('${getDocumentsDirPath()}/counter.txt');
+  final useFlutterDriver = value == 'true' || DRIVE_MODE;
+
+  print("The value is $value");
 
   if (!useFlutterDriver) {
     WidgetsFlutterBinding.ensureInitialized();
@@ -45,11 +52,19 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  @override
+  void initState() {
+    super.initState();
+    checkDocumentsForFileIfItExistsSaveToTemp('counter.txt');
+  }
+
   void _incrementCounter() {
     // TASK: Change the value here and save
     setState(() {
       _counter++;
     });
+    bool state = _counter % 2 == 0;
+    writeToTile('counter.txt', state.toString());
   }
 
   @override
@@ -79,5 +94,36 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+}
+
+String getDocumentsDirPath() {
+  return Directory.systemTemp.path;
+}
+
+String readValueSync(String filePath) {
+  final file = File(filePath);
+  return file.existsSync() ? file.readAsStringSync() : 'false';
+}
+
+void writeToTile(String fileName, String fileData) async {
+  final file = File('${getDocumentsDirPath()}/$fileName');
+  file.writeAsStringSync(fileData);
+
+  // // Write to actual documents
+  final appDocuments = await getApplicationDocumentsDirectory();
+  final appDocumentsDir = appDocuments.path;
+  final appFile = File('$appDocumentsDir/$fileName');
+  appFile.writeAsStringSync(fileData);
+}
+
+void checkDocumentsForFileIfItExistsSaveToTemp(String fileString) async {
+  final appDocuments = await getApplicationDocumentsDirectory();
+  final appDocumentsDir = appDocuments.path;
+  final appFile = File('$appDocumentsDir/$fileString');
+  final tempFile = File('${getDocumentsDirPath()}/$fileString');
+  if (appFile.existsSync() && !tempFile.existsSync()) {
+    final file = File('${getDocumentsDirPath()}/$fileString');
+    file.writeAsStringSync(appFile.readAsStringSync());
   }
 }
